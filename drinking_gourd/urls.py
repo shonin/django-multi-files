453155b0b420aa1b49bda4e7f5973e20f6b854c7
Elliot Charney <elliot.charney@ithaka.org>
2016-09-28 15:13:50 -0400

    Add delete option

--- drinking_gourd/urls.py
+++ drinking_gourd/urls.py
@@ -4,5 +4,6 @@ from drinking_gourd import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^edit/(?P<pk>[0-9]+)/', views.EditFileView.as_view(), name='edit_file'),
+    url(r'^delete/(?P<pk>[0-9]+)/', views.DeleteFileView.as_view(), name='delete_file'),
     url(r'^upload', views.UploadView.as_view(), name='upload'),
 ]
--- drinking_gourd/views.py
+++ drinking_gourd/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import FormView, UpdateView
+from django.views.generic.edit import FormView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-import boto3
+from django.http import HttpResponseRedirect
 
 from drinking_gourd.models import File
-from drinking_gourd.forms import UploadForm, EditFileForm
-from drinking_gourd.helpers.s3_helper import upload_file
+from drinking_gourd.forms import UploadForm
+from drinking_gourd.helpers.s3_helper import upload_file, delete_file
 
 
 @login_required(login_url="login/")
@@ -16,12 +15,16 @@ def home(request):
     return render(request,"home.html", {'files':files})
 
 class EditFileView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+
     model = File
     fields = ['name', 'description']
     template_name = 'edit_file.html'
     success_url = '/drinkinggourd/'
 
 class UploadView(LoginRequiredMixin, FormView):
+    login_url = '/login/'
+
     template_name = 'uploads/form.html'
     form_class = UploadForm
     success_url = '/drinkinggourd/'
@@ -33,3 +36,17 @@ class UploadView(LoginRequiredMixin, FormView):
             upload_file(file, file_object.key)
 
         return super(UploadView, self).form_valid(form)
+
+class DeleteFileView(LoginRequiredMixin, DeleteView):
+    login_url = '/login/'
+
+    model = File
+    template_name = 'delete_file.html'
+    success_url = '/drinkinggourd/'
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+        self.object.delete()
+        delete_file(self.object.key)
+        return HttpResponseRedirect(success_url)
